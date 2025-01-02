4ba84bd583fbd67ec5decd36728620271eb0136f
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-01-02 13:28:29 +0100

    Füge Authentifizierungs-Komponente hinzu: Implementiere Login- und Registrierungsformular in der Auth-Komponente und aktualisiere App-Komponente zur Bedingung der Anzeige

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,14 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+//!COMPONENTS
 import ListHeader from "./components/ListHeader";
 import ListItem from "./components/ListItem";
+import Auth from "./components/Auth";
 
 const App = () => {
   const userEmail = "ali@test.com";
   const [tasks, setTasks] = useState(null);
+  const authToken = false;
 
   const getData = async () => {
     try {
@@ -26,12 +29,17 @@ const App = () => {
 
   return (
     <div className="app">
-      <ListHeader listName={"ToDo Spark"}  getData={getData}/>
-      {sortedTask?.map((task) => (
-        <ListItem key={task.id} task={task} getData={getData}/>
-      ))}
+      {!authToken && <Auth/>}
+      {authToken && (
+        <>
+          <ListHeader listName={"ToDo Spark"} getData={getData} />
+          {sortedTask?.map((task) => (
+            <ListItem key={task.id} task={task} getData={getData} />
+          ))}
+        </>
+      )}
     </div>
   );
-};
+}
 
 export default App;
--- client/src/components/Auth.jsx
+++ client/src/components/Auth.jsx
@@ -1,7 +1,35 @@
+import { useState } from "react";
+
 const Auth = () => {
-    return<div>
-        <h1>Auth</h1>
+    const [error, setError] = useState(null);
+    const [isLogin, setIsLogin] = useState(false);
+    const viewLogin = (status) => {
+        setIsLogin(status);
+        setError(null);
+    }
+
+    return(
+    <div className="auth-container">
+        <div className="auth-container-box">
+            <form>
+                <h2>{isLogin ? 'Please log in' : 'Please sign up!'}</h2>
+                <input type="email" placeholder="email"/>
+                <input type="password" placeholder="password"/>
+                {!isLogin && <input type="password" placeholder="confirm password"/>}
+                <input type="submit" className="create" />
+                { error && <p>{error}</p>}
+            </form>
+            <div className="auth-options">
+                <button onClick={() => viewLogin(false)}
+                    style={{backgroundColor : !isLogin ? '#f9f9f9' : 'rgb(188, 188, 188)'}}
+                    >Sign Up</button>
+                <button onClick={() => viewLogin(true)}
+                    style={{backgroundColor : !isLogin ? '#f9f9f9' : 'rgb(188, 188, 188)'}}
+                    >Login</button>
+            </div>
+        </div>
     </div>
+    )
 }
 
 export default Auth;
