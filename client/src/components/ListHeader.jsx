71be82caa3e9b6a389765499984c07724aef1e36
Ali Ramazan Yildirim <aliramazanyildirim@gmail.com>
2025-02-10 18:20:35 +0100

    Integriere signOut- und handleAdd-Funktionen in useModalStore; vereinfache ListHeader-Komponente durch Nutzung des neuen Stores

--- client/src/components/ListHeader.jsx
+++ client/src/components/ListHeader.jsx
@@ -1,22 +1,8 @@
-import { useCookies } from "react-cookie";
 import useModalStore from "../store/useModalStore";
 import Modal from "./Modal";
 
 const ListHeader = () => {
-  const { showModal, setShowModal, setMode } = useModalStore();
-  const [, , removeCookie] = useCookies(null);
-
-  const signOut = () => {
-    console.log('Signing out...');
-    removeCookie('Email');
-    removeCookie('Token');
-    window.location.reload();
-  };
-
-  const handleAdd = () => {
-    setMode("create");
-    setShowModal(true);
-  };
+  const { showModal, signOut,handleAdd } = useModalStore();
 
   return (
     <div className="list-header">
--- client/src/store/useModalStore.js
+++ client/src/store/useModalStore.js
@@ -1,21 +1,36 @@
-import {create} from 'zustand';
+import { create } from 'zustand';
 
-const useModalStore = create((set) => ({
-  data: {
-    user_email: '',
-    title: '',
-    progress: 50,
-    date: new Date(),
-  },
-  setData: (newData) => set((state) => ({ data: { ...state.data, ...newData } })),
-  showModal: false,
-  setShowModal: (show) => set({ showModal: show }),
-  mode: '',
-  setMode: (mode) => set({ mode }),
-  task: null,
-  setTask: (task) => set({ task }),
-  getData: () => {},
-  setGetData: (getData) => set({ getData }),
-}));
+const useModalStore = create((set) => {
+  const signOut = (removeCookie) => {
+    console.log('Signing out...');
+    removeCookie('Email');
+    removeCookie('Token');
+    window.location.reload();
+  };
+
+  const handleAdd = () => {
+    set((state) => ({ ...state, mode: "create", showModal: true }));
+  };
+
+  return {
+    data: {
+      user_email: '',
+      title: '',
+      progress: 50,
+      date: new Date(),
+    },
+    setData: (newData) => set((state) => ({ data: { ...state.data, ...newData } })),
+    showModal: false,
+    setShowModal: (show) => set({ showModal: show }),
+    mode: '',
+    setMode: (mode) => set({ mode }),
+    task: null,
+    setTask: (task) => set({ task }),
+    getData: () => {},
+    setGetData: (getData) => set({ getData }),
+    signOut,
+    handleAdd,
+  };
+});
 
 export default useModalStore;
